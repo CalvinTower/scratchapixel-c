a86b6364c7a31f348248aaa8437bda7aa6f19c71
Calvin Tower <Calvin@CorianderDesigns.com>
2018-11-05 09:05:12 -0800

    Add multiply function to Matrix
    Good Lord was it an ordeal.

--- src/Matrix.c
+++ src/Matrix.c
@@ -31,7 +31,7 @@ Matrix_float *Matrix_float_init(int dimension)
   return out;
 }
 
-float Matrix_float_get_entry(Matrix_float *in, int x, int y, int *error)
+float Matrix_float_get_entry(Matrix_float const *in, int x, int y, int *error)
 {
   if(x > (in->dimension - 1) || y > (in->dimension - 1)) {
     *error = 1;
@@ -46,11 +46,34 @@ void Matrix_float_set_entry(Matrix_float *in, float value, int x, int y, int *er
 {
   if(x > (in->dimension - 1) || y > (in->dimension - 1)) {
     *error = 1;
-    return 0;
+    return;
   }
   y *= in->dimension;
   *(in->entries + x + y) = value;
   *error = 0;
+}
+
+Matrix_float *Matrix_float_multiply(Matrix_float const *a, Matrix_float const *b)
+{
+  if(a->dimension != b->dimension) {
+    return NULL;
+  }
+  Matrix_float *out = Matrix_float_init(a->dimension);
+  if(out == NULL) {
+    return NULL;
+  }
+
+  for(int y = 0; y < out->dimension; y++) {
+    for(int x = 0; x < out->dimension; x++) {
+      float value = 0;
+      for(int i = 0; i < out->dimension; i++) {
+        value +=
+          *(a->entries + x + (i * out->dimension)) * *(b->entries + i + (y * out->dimension));
+      }
+      Matrix_float_set_entry(out, value, x, y, NULL);
+    }
+  }
+  return out;
 }
 
 void Matrix_float_destroy(Matrix_float *in)
@@ -90,7 +113,7 @@ Matrix_double *Matrix_double_init(int dimension)
   return out;
 }
 
-double Matrix_double_get_entry(Matrix_double *in, int x, int y, int *error)
+double Matrix_double_get_entry(Matrix_double const *in, int x, int y, int *error)
 {
   if(x > (in->dimension - 1) || y > (in->dimension - 1)) {
     *error = 1;
@@ -105,11 +128,34 @@ void Matrix_double_set_entry(Matrix_double *in, double value, int x, int y, int 
 {
   if(x > (in->dimension - 1) || y > (in->dimension - 1)) {
     *error = 1;
-    return 0;
+    return;
   }
   y *= in->dimension;
   *(in->entries + x + y) = value;
   *error = 0;
+}
+
+Matrix_double *Matrix_double_multiply(Matrix_double const *a, Matrix_double const *b)
+{
+  if(a->dimension != b->dimension) {
+    return NULL;
+  }
+  Matrix_double *out = Matrix_double_init(a->dimension);
+  if(out == NULL) {
+    return NULL;
+  }
+
+  for(int y = 0; y < out->dimension; y++) {
+    for(int x = 0; x < out->dimension; x++) {
+      double value = 0;
+      for(int i = 0; i < out->dimension; i++) {
+        value +=
+          *(a->entries + x + (i * out->dimension)) * *(b->entries + i + (y * out->dimension));
+      }
+      Matrix_double_set_entry(out, value, x, y, NULL);
+    }
+  }
+  return out;
 }
 
 void Matrix_double_destroy(Matrix_double *in)
@@ -149,7 +195,7 @@ Matrix_int *Matrix_int_init(int dimension)
   return out;
 }
 
-int Matrix_int_get_entry(Matrix_int *in, int x, int y, int *error)
+int Matrix_int_get_entry(Matrix_int const *in, int x, int y, int *error)
 {
   if(x > (in->dimension - 1) || y > (in->dimension - 1)) {
     *error = 1;
@@ -164,11 +210,40 @@ void Matrix_int_set_entry(Matrix_int *in, int value, int x, int y, int *error)
 {
   if(x > (in->dimension - 1) || y > (in->dimension - 1)) {
     *error = 1;
-    return 0;
+    return;
   }
   y *= in->dimension;
   *(in->entries + x + y) = value;
   *error = 0;
+}
+
+Matrix_int *Matrix_int_multiply(Matrix_int const *a, Matrix_int const *b)
+{
+  if(a->dimension != b->dimension) {
+    return NULL;
+  }
+  Matrix_int *out = Matrix_int_init(a->dimension);
+  if(out == NULL) {
+    return NULL;
+  }
+  /* This ridiculously nested loop is unbelievably verbose. I've
+     heard jokes about how everyone thinks they can write the new
+     fastest matrix multiplication algorithm, but really they should
+     just use a library, and now I understand those jokes. I should
+     really just use a library for this, but that kind of defeats the
+     purpose of this project. */
+
+  for(int y = 0; y < out->dimension; y++) {
+    for(int x = 0; x < out->dimension; x++) {
+      int value = 0;
+      for(int i = 0; i < out->dimension; i++) {
+        value +=
+          *(a->entries + x + (i * out->dimension)) * *(b->entries + i + (y * out->dimension));
+      }
+      Matrix_int_set_entry(out, value, x, y, NULL);
+    }
+  }
+  return out;
 }
 
 void Matrix_int_destroy(Matrix_int *in)
