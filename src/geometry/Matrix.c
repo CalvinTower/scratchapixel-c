055e6ff5e0e543ea82d8be509397978ba66e1897
Calvin Tower <Calvin@CorianderDesigns.com>
2018-11-06 08:24:19 -0800

    Change directory structure slightly

---
+++ src/geometry/Matrix.c
@@ -0,0 +1,317 @@
+#include "Matrix.h"
+#include <stdlib.h>
+
+/*
+  ================================================================================
+  Matrix_float functions
+  ================================================================================
+*/
+
+Matrix_float *Matrix_float_init(int dimension)
+{
+  if(dimension > 4 || dimension < 2) {
+    return NULL;
+  }
+  Matrix_float *out = malloc(sizeof(Matrix_float));
+  if(out == NULL) {
+    return NULL;
+  }
+
+  out->entries = malloc(sizeof(float) * dimension * dimension);
+  if(out->entries == NULL) {
+    free(out);
+    return NULL;
+  }
+
+  float *i = out->entries;
+  for(int j = 0; j > dimension * dimension; j++) {
+    *i = 0;
+  }
+
+  return out;
+}
+
+int Matrix_float_get_dimension(Matrix_float const *in)
+{
+  return in->dimension;
+}
+
+float Matrix_float_get_entry(Matrix_float const *in, int x, int y, int *error)
+{
+  if(x > (in->dimension - 1) || y > (in->dimension - 1)) {
+    *error = 1;
+    return 0;
+  }
+  y *= in->dimension;
+  *error = 0;
+  return *(in->entries + x + y);
+}
+
+void Matrix_float_set_entry(Matrix_float *in, float value, int x, int y, int *error)
+{
+  if(x > (in->dimension - 1) || y > (in->dimension - 1)) {
+    *error = 1;
+    return;
+  }
+  y *= in->dimension;
+  *(in->entries + x + y) = value;
+  *error = 0;
+}
+
+Matrix_float *Matrix_float_multiply(Matrix_float const *a, Matrix_float const *b)
+{
+  if(a->dimension != b->dimension) {
+    return NULL;
+  }
+  Matrix_float *out = Matrix_float_init(a->dimension);
+  if(out == NULL) {
+    return NULL;
+  }
+
+  for(int y = 0; y < out->dimension; y++) {
+    for(int x = 0; x < out->dimension; x++) {
+      float value = 0;
+      for(int i = 0; i < out->dimension; i++) {
+        value +=
+          *(a->entries + x + (i * out->dimension)) * *(b->entries + i + (y * out->dimension));
+      }
+      Matrix_float_set_entry(out, value, x, y, NULL);
+    }
+  }
+  return out;
+}
+
+Matrix_float *Matrix_float_transpose(Matrix_float const *in)
+{
+  Matrix_float *out Matrix_float_init(in->dimension);
+  if(out == NULL) {
+    return NULL;
+  }
+
+  int dim = in->dimension;
+  for(int y = 0; y < dim; y++) {
+    for(int x = 0; x < dim; x++) {
+      Matrix_float_set_entry(out, Matrix_float_get_entry(in, x, y, NULL), x, y, NULL);
+    }
+  }
+  return out;
+}
+
+void Matrix_float_destroy(Matrix_float *in)
+{
+  free(in->entries);
+  free(in);
+  in = NULL;
+}
+
+/*
+  ================================================================================
+  Matrix_double functions
+  ================================================================================
+*/
+
+Matrix_double *Matrix_double_init(int dimension)
+{
+  if(dimension > 4 || dimension < 2) {
+    return NULL;
+  }
+  Matrix_double *out = malloc(sizeof(Matrix_double));
+  if(out == NULL) {
+    return NULL;
+  }
+
+  out->entries = malloc(sizeof(double) * dimension * dimension);
+  if(out->entries == NULL) {
+    free(out);
+    return NULL;
+  }
+
+  double *i = out->entries;
+  for(int j = 0; j > dimension * dimension; j++) {
+    *i = 0;
+  }
+
+  return out;
+}
+
+int Matrix_double_get_dimension(Matrix_double const *in)
+{
+  return in->dimension;
+}
+
+double Matrix_double_get_entry(Matrix_double const *in, int x, int y, int *error)
+{
+  if(x > (in->dimension - 1) || y > (in->dimension - 1)) {
+    *error = 1;
+    return 0;
+  }
+  y *= in->dimension;
+  *error = 0;
+  return *(in->entries + x + y);
+}
+
+void Matrix_double_set_entry(Matrix_double *in, double value, int x, int y, int *error)
+{
+  if(x > (in->dimension - 1) || y > (in->dimension - 1)) {
+    *error = 1;
+    return;
+  }
+  y *= in->dimension;
+  *(in->entries + x + y) = value;
+  *error = 0;
+}
+
+Matrix_double *Matrix_double_multiply(Matrix_double const *a, Matrix_double const *b)
+{
+  if(a->dimension != b->dimension) {
+    return NULL;
+  }
+  Matrix_double *out = Matrix_double_init(a->dimension);
+  if(out == NULL) {
+    return NULL;
+  }
+
+  for(int y = 0; y < out->dimension; y++) {
+    for(int x = 0; x < out->dimension; x++) {
+      double value = 0;
+      for(int i = 0; i < out->dimension; i++) {
+        value +=
+          *(a->entries + x + (i * out->dimension)) * *(b->entries + i + (y * out->dimension));
+      }
+      Matrix_double_set_entry(out, value, x, y, NULL);
+    }
+  }
+  return out;
+}
+
+Matrix_double *Matrix_double_transpose(Matrix_double const *in)
+{
+  Matrix_double *out Matrix_double_init(in->dimension);
+  if(out == NULL) {
+    return NULL;
+  }
+
+  int dim = in->dimension;
+  for(int y = 0; y < dim; y++) {
+    for(int x = 0; x < dim; x++) {
+      Matrix_double_set_entry(out, Matrix_double_get_entry(in, x, y, NULL), x, y, NULL);
+    }
+  }
+  return out;
+}
+
+void Matrix_double_destroy(Matrix_double *in)
+{
+  free(in->entries);
+  free(in);
+  in = NULL;
+}
+
+/*
+  ================================================================================
+  Matrix_int functions
+  ================================================================================
+*/
+
+Matrix_int *Matrix_int_init(int dimension)
+{
+  if(dimension > 4 || dimension < 2) {
+    return NULL;
+  }
+  Matrix_int *out = malloc(sizeof(Matrix_int));
+  if(out == NULL) {
+    return NULL;
+  }
+
+  out->entries = malloc(sizeof(int) * dimension * dimension);
+  if(out->entries == NULL) {
+    free(out);
+    return NULL;
+  }
+
+  int *i = out->entries;
+  for(int j = 0; j > dimension * dimension; j++) {
+    *i = 0;
+  }
+
+  return out;
+}
+
+int Matrix_int_get_dimension(Matrix_int const *in)
+{
+  return in->dimension;
+}
+
+int Matrix_int_get_entry(Matrix_int const *in, int x, int y, int *error)
+{
+  if(x > (in->dimension - 1) || y > (in->dimension - 1)) {
+    *error = 1;
+    return 0;
+  }
+  y *= in->dimension;
+  *error = 0;
+  return *(in->entries + x + y);
+}
+
+void Matrix_int_set_entry(Matrix_int *in, int value, int x, int y, int *error)
+{
+  if(x > (in->dimension - 1) || y > (in->dimension - 1)) {
+    *error = 1;
+    return;
+  }
+  y *= in->dimension;
+  *(in->entries + x + y) = value;
+  *error = 0;
+}
+
+Matrix_int *Matrix_int_multiply(Matrix_int const *a, Matrix_int const *b)
+{
+  if(a->dimension != b->dimension) {
+    return NULL;
+  }
+  Matrix_int *out = Matrix_int_init(a->dimension);
+  if(out == NULL) {
+    return NULL;
+  }
+  /* This ridiculously nested loop is unbelievably verbose. I've
+     heard jokes about how everyone thinks they can write the new
+     fastest matrix multiplication algorithm, but really they should
+     just use a library, and now I understand those jokes. I should
+     really just use a library for this, but that kind of defeats the
+     purpose of this project. */
+
+  for(int y = 0; y < out->dimension; y++) {
+    for(int x = 0; x < out->dimension; x++) {
+      int value = 0;
+      for(int i = 0; i < out->dimension; i++) {
+        value +=
+          *(a->entries + x + (i * out->dimension)) * *(b->entries + i + (y * out->dimension));
+      }
+      Matrix_int_set_entry(out, value, x, y, NULL);
+    }
+  }
+  return out;
+}
+
+Matrix_int *Matrix_int_transpose(Matrix_int const *in)
+{
+  Matrix_int *out Matrix_int_init(in->dimension);
+  if(out == NULL) {
+    return NULL;
+  }
+
+  int dim = in->dimension;
+  for(int y = 0; y < dim; y++) {
+    for(int x = 0; x < dim; x++) {
+      Matrix_int_set_entry(out, Matrix_int_get_entry(in, x, y, NULL), x, y, NULL);
+    }
+  }
+  return out;
+}
+
+void Matrix_int_destroy(Matrix_int *in)
+{
+  free(in->entries);
+  free(in);
+  in = NULL;
+}
---
+++ src/geometry/Vector.c
@@ -0,0 +1,399 @@
+#include "Vector.h"
+#include <math.h>
+#include <stdlib.h>
+
+/*
+  ================================================================================
+  Vec3_float functions
+  ================================================================================
+*/
+
+Vec3_float *Vec3_float_init(float x, float y, float z)
+{
+  Vec3_float *res = malloc(sizeof(Vec3_float));
+  if(res == NULL) {
+    return NULL;
+  }
+  res->x = x;
+  res->y = y;
+  res->z = z;
+
+  return res;
+}
+
+double Vec3_float_length(Vec3_float const *in)
+{
+  return sqrt((in->x * in->x) + (in->y * in->y) + (in->z * in->z));
+}
+
+Vec3_float *Vec3_float_add(Vec3_float const *a, Vec3_float const *b)
+{
+  Vec3_float *out = Vec3_float_init(a->x + b->x,
+                                    a->y + b->y,
+                                    a->z + b->z);
+  if(out == NULL) {
+    return NULL;
+  }
+  return out;
+}
+
+Vec3_float *Vec3_float_subtract(Vec3_float const *a, Vec3_float const *b)
+{
+  Vec3_float *out = Vec3_float_init(a->x - b->x,
+                                    a->y - b->y,
+                                    a->z - b->z);
+  if(out == NULL) {
+    return NULL;
+  }
+  return out;
+}
+
+Vec3_float *Vec3_float_multiply(Vec3_float const *in, float scale)
+{
+  Vec3_float *out = Vec3_float_init(in->x * scale,
+                                    in->y * scale,
+                                    in->z * scale);
+  if(out == NULL) {
+    return NULL;
+  }
+  return out;
+}
+
+Vec3_float *Vec3_float_normalize(Vec3_float const *in)
+{
+  double magnitude = Vec3_float_length(in);
+  if(magnitude == 0) {
+    return NULL;
+  }
+  /* Create a temporary variable for the inverted magnitude of the
+     incoming vector; multiplication is faster, and should be favored
+     for such a commonly used function */
+  double inverted_magnitude = 1 / magnitude;
+
+  /* I'm not sure if multiplying by a double will leave it promoted
+     to a double when it gets passed to the funciton, so I'm giving
+     it an explicit cast for safety */
+  Vec3_float *out = Vec3_float_init((float) in->x * inverted_magnitude,
+                                    (float) in->y * inverted_magnitude,
+                                    (float) in->z * inverted_magnitude);
+  if(out == NULL) {
+    return NULL;
+  }
+  return out;
+}
+
+float Vec3_float_dot(Vec3_float const *a, Vec3_float const *b)
+{
+  return (a->x * b->x) + (a->y * b->y) + (a->z * b->z);
+}
+
+Vec3_float *Vec3_float_cross(Vec3_float const *a, Vec3_float const *b)
+{
+  /* You'd better believe that I'll never in my life remember the
+     order for this algorithm */
+  float x, y, z;
+  x = (a->y * b->z) - (a->z * b->y);
+  y = (a->z * b->x) - (a->x * b->z);
+  z = (a->x * b->y) - (a->y * b->x);
+
+  Vec3_float *out = Vec3_float_init(x, y, z);
+  /* Returning it as-is would be exactly the same without this next
+     check, since if it comes out NULL then it would be returning
+     NULL, but I want the check there for clarity. It's good practice. */
+  if(out == NULL) {
+    return NULL;
+  }
+  return out;
+}
+
+Vec3_float *Vec3_Matrix_float_multiply(Vec3_float const *vec, Matrix_float const *mat)
+{
+  if(Matrix_float_get_dimension(mat) != 3) {
+    return NULL;
+  }
+  /* DUMBEST VERSION */
+  float x_out, y_out, z_out;
+  x_out = (vec->x * Matrix_float_get_entry(mat, 0, 0, NULL)) +
+    (vec->y * Matrix_float_get_entry(mat, 1, 0, NULL)) +
+    (vec->z * Matrix_float_get_entry(mat, 2, 0, NULL));
+
+  y_out = (vec->x * Matrix_float_get_entry(mat, 0, 1, NULL)) +
+    (vec->y * Matrix_float_get_entry(mat, 1, 1, NULL)) +
+    (vec->z * Matrix_float_get_entry(mat, 2, 1, NULL));
+
+  z_out = (vec->x * Matrix_float_get_entry(mat, 0, 2, NULL)) +
+    (vec->y * Matrix_float_get_entry(mat, 1, 2, NULL)) +
+    (vec->z * Matrix_float_get_entry(mat, 2, 2, NULL));
+
+  Vec3_float *out = Vec3_float_init(x_out, y_out, z_out);
+  if(out == NULL) {
+    return NULL;
+  }
+  return out;
+}
+
+void Vec3_float_destroy(Vec3_float *in)
+{
+  free(in);
+  in = NULL;
+}
+
+/*
+  ================================================================================
+  Vec3_double functions
+  ================================================================================
+*/
+
+Vec3_double *Vec3_double_init(double x, double y, double z)
+{
+  Vec3_double *res = malloc(sizeof(Vec3_double));
+  if(res == NULL) {
+    return NULL;
+  }
+  res->x = x;
+  res->y = y;
+  res->z = z;
+
+  return res;
+}
+
+double Vec3_double_length(Vec3_double const *in)
+{
+  return sqrt((in->x * in->x) + (in->y * in->y) + (in->z * in->z));
+}
+
+Vec3_double *Vec3_double_add(Vec3_double const *a, Vec3_double const *b)
+{
+  Vec3_double *out = Vec3_double_init(a->x + b->x,
+                                      a->y + b->y,
+                                      a->z + b->z);
+  if(out == NULL) {
+    return NULL;
+  }
+  return out;
+}
+
+Vec3_double *Vec3_double_subtract(Vec3_double const *a, Vec3_double const *b)
+{
+  Vec3_double *out = Vec3_double_init(a->x - b->x,
+                                      a->y - b->y,
+                                      a->z - b->z);
+  if(out == NULL) {
+    return NULL;
+  }
+  return out;
+}
+
+Vec3_double *Vec3_double_multiply(Vec3_double const *in, double scale)
+{
+  Vec3_double *out = Vec3_double_init(in->x * scale,
+                                      in->y * scale,
+                                      in->z * scale);
+  if(out == NULL) {
+    return NULL;
+  }
+  return out;
+}
+
+Vec3_double *Vec3_double_normalize(Vec3_double const *in)
+{
+  double magnitude = Vec3_double_length(in);
+  if(magnitude == 0) {
+    return NULL;
+  }
+  /* "Maybe this is premature optimization", I hear you say. "Creating
+     a temporary variable to use multiplication instead of division
+     obfuscates your code and makes it less readable" Yeah sure, but
+     the instructional material has me do it explicitly, so I'm going
+     for it. */
+  double inverted_magnitude = 1 / magnitude;
+
+  Vec3_double *out = Vec3_double_init(in->x * inverted_magnitude,
+                                      in->y * inverted_magnitude,
+                                      in->z * inverted_magnitude);
+  if(out == NULL) {
+    return NULL;
+  }
+  return out;
+}
+
+double Vec3_double_dot(Vec3_double const *a, Vec3_double const *b)
+{
+  return (a->x * b->x) + (a->y * b->y) + (a->z * b->z);
+}
+
+Vec3_double *Vec3_double_cross(Vec3_double const *a, Vec3_double const *b)
+{
+  double x, y, z;
+  x = (a->y * b->z) - (a->z * b->y);
+  y = (a->z * b->x) - (a->x * b->z);
+  z = (a->x * b->y) - (a->y * b->x);
+
+  Vec3_double *out = Vec3_double_init(x, y, z);
+  if(out == NULL) {
+    return NULL;
+  }
+  return out;
+}
+
+Vec3_double *Vec3_Matrix_double_multiply(Vec3_double const *vec, Matrix_double const *mat)
+{
+  if(Matrix_double_get_dimension(mat) != 3) {
+    return NULL;
+  }
+  /* DUMBEST VERSION */
+  double x_out, y_out, z_out;
+  x_out = (vec->x * Matrix_double_get_entry(mat, 0, 0, NULL)) +
+    (vec->y * Matrix_double_get_entry(mat, 1, 0, NULL)) +
+    (vec->z * Matrix_double_get_entry(mat, 2, 0, NULL));
+
+  y_out = (vec->x * Matrix_double_get_entry(mat, 0, 1, NULL)) +
+    (vec->y * Matrix_double_get_entry(mat, 1, 1, NULL)) +
+    (vec->z * Matrix_double_get_entry(mat, 2, 1, NULL));
+
+  z_out = (vec->x * Matrix_double_get_entry(mat, 0, 2, NULL)) +
+    (vec->y * Matrix_double_get_entry(mat, 1, 2, NULL)) +
+    (vec->z * Matrix_double_get_entry(mat, 2, 2, NULL));
+
+  Vec3_double *out = Vec3_double_init(x_out, y_out, z_out);
+  if(out == NULL) {
+    return NULL;
+  }
+  return out;
+}
+
+void Vec3_double_destroy(Vec3_double *in)
+{
+  free(in);
+  in = NULL;
+}
+
+/*
+  ================================================================================
+  Vec3_int functions
+  ================================================================================
+*/
+
+Vec3_int *Vec3_int_init(int x, int y, int z)
+{
+  Vec3_int *res = malloc(sizeof(Vec3_int));
+  if(res == NULL) {
+    return NULL;
+  }
+  res->x = x;
+  res->y = y;
+  res->z = z;
+
+  return res;
+}
+
+double Vec3_int_length(Vec3_int const *in)
+{
+  return sqrt((in->x * in->x) + (in->y * in->y) + (in->z * in->z));
+}
+
+Vec3_int *Vec3_int_add(Vec3_int const *a, Vec3_int const *b)
+{
+  Vec3_int *out = Vec3_int_init(a->x + b->x,
+                                a->y + b->y,
+                                a->z + b->z);
+  if(out == NULL) {
+    return NULL;
+  }
+  return out;
+}
+
+Vec3_int *Vec3_int_subtract(Vec3_int const *a, Vec3_int const *b)
+{
+  Vec3_int *out = Vec3_int_init(a->x - b->x,
+                                a->y - b->y,
+                                a->z - b->z);
+  if(out == NULL) {
+    return NULL;
+  }
+  return out;
+}
+
+Vec3_int *Vec3_int_multiply(Vec3_int const *in, int scale)
+{
+  Vec3_int *out = Vec3_int_init(in->x * scale,
+                                in->y * scale,
+                                in->z * scale);
+  if(out == NULL) {
+    return NULL;
+  }
+  return out;
+}
+
+/* DANGER This function returns a Vec3_float *, not a Vec3_int *
+   It returns the float version because in terms of amount of
+   precision in a data type, float is closer to int than double. */
+Vec3_float *Vec3_int_normalize(Vec3_int const *in)
+{
+  double magnitude = Vec3_int_length(in);
+  if(magnitude == 0) {
+    return NULL;
+  }
+  /* Again we create the temporary variable */
+  double inverted_magnitude = (1 / Vec3_int_length(in));
+
+  /* And again the explcit casts */
+  Vec3_float *out = Vec3_float_init((float) in->x * inverted_magnitude,
+                                    (float) in->y * inverted_magnitude,
+                                    (float) in->z * inverted_magnitude);
+  if(out == NULL) {
+    return NULL;
+  }
+  return out;
+}
+
+int Vec3_int_dot(Vec3_int const *a, Vec3_int const *b)
+{
+  return (a->x * b->x) + (a->y * b->y) + (a->z * b->z);
+}
+
+Vec3_int *Vec3_int_cross(Vec3_int const *a, Vec3_int const *b)
+{
+  int x, y, z;
+  x = (a->y * b->z) - (a->z * b->y);
+  y = (a->z * b->x) - (a->x * b->z);
+  z = (a->x * b->y) - (a->y * b->x);
+
+  Vec3_int *out = Vec3_int_init(x, y, z);
+  if(out == NULL) {
+    return NULL;
+  }
+  return out;
+}
+
+Vec3_int *Vec3_Matrix_int_multiply(Vec3_int const *vec, Matrix_int const *mat)
+{
+  if(Matrix_int_get_dimension(mat) != 3) {
+    return NULL;
+  }
+  /* DUMBEST VERSION */
+  int x_out, y_out, z_out;
+  x_out = (vec->x * Matrix_int_get_entry(mat, 0, 0, NULL)) +
+    (vec->y * Matrix_int_get_entry(mat, 1, 0, NULL)) +
+    (vec->z * Matrix_int_get_entry(mat, 2, 0, NULL));
+
+  y_out = (vec->x * Matrix_int_get_entry(mat, 0, 1, NULL)) +
+    (vec->y * Matrix_int_get_entry(mat, 1, 1, NULL)) +
+    (vec->z * Matrix_int_get_entry(mat, 2, 1, NULL));
+
+  z_out = (vec->x * Matrix_int_get_entry(mat, 0, 2, NULL)) +
+    (vec->y * Matrix_int_get_entry(mat, 1, 2, NULL)) +
+    (vec->z * Matrix_int_get_entry(mat, 2, 2, NULL));
+
+  Vec3_int *out = Vec3_int_init(x_out, y_out, z_out);
+  if(out == NULL) {
+    return NULL;
+  }
+  return out;
+}
+
+void Vec3_int_destroy(Vec3_int *in)
+{
+  free(in);
+  in = NULL;
+}
